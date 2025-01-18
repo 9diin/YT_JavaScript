eb5efef2a177693f5ffa50c4a95bd63f17accf1c
9diin <weartstudio@naver.com>
2025-01-18 14:15:16 +0900

    🔥 [Feat] 05. 자바스크립트 조건문 - if else, switch

---
+++ ch02/switch.js
@@ -0,0 +1,17 @@
+let drink = prompt("가격 조회를 원하는 음료수를 입력하세요. (콜라, 사이다, 커피 中 택1)", "");
+
+switch (drink) {
+    case "콜라":
+        console.log("1,000원 입니다.");
+        break;
+    case "사이다":
+        console.log("2,000원 입니다.");
+        break;
+    case "커피":
+        console.log("3,000원 입니다.");
+        break;
+
+    default:
+        console.log("잘못된 단어 입력");
+        break;
+}

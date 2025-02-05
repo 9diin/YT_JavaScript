f4a76b3e3af99254fb1bd937b212e7a408d4c3ac
9diin <weartstudio@naver.com>
2025-02-06 08:43:36 +0900

    🔥 [Feat] 06. 자바스크립트 반복문 - while, do-while, for

--- ch02/for.js
+++ ch02/for.js
@@ -1,5 +1,4 @@
 // 3. for
-
 // for 문은 자바스크립트의 대표적인 반복문
 
 for (initialize; test; increment) {
@@ -7,12 +6,13 @@ for (initialize; test; increment) {
 }
 
 // initialize, test, increment는 세미콜론으로 구분하며 각각 루프 변수의 초기화, 테스트, 증감을 담당합니다.
-// 이 요소들은 루프의 첫 번째 행에 모음으로써 for 루프가 무엇을 하는지 이해하기 쉽고, 루프 변수의 초기화나 증감을 잊어버리는 실수를 방지할 수 있다는 편리함이 있습니다.
+// 이 요소들은 루프의 첫 번째 행에 모음으로써 for 루프가 무엇을 하는지 이해하기 쉽고,
+// 루프 변수의 초기화나 증감을 잊어버리는 실수를 방지할 수 있다는 편리함이 있습니다.
 // while 문과 비교해볼까요?
 
 initialize;
 while (test) {
-    statement;
+    statment;
     increment;
 }
 
